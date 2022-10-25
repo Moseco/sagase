c3b9657fa26f1fb965eb9f971c392a324ca7dd47
Moseco <hampus_hammarlund@hotmail.com>
2022-10-25 14:48:00 +0900

    Fixed list padding

--- lib/ui/views/kanji_radicals/kanji_radicals_view.dart
+++ lib/ui/views/kanji_radicals/kanji_radicals_view.dart
@@ -44,36 +44,28 @@ class KanjiRadicalsView extends StatelessWidget {
 
         return Scaffold(
           appBar: AppBar(title: const Text('Radicals')),
-          body: ListView(
-            children: [
-              ListView.builder(
-                primary: false,
-                shrinkWrap: true,
-                itemCount: radicalGroups.length,
-                itemBuilder: (context, groupIndex) => Padding(
-                  padding: const EdgeInsets.only(bottom: 4),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        color: Colors.deepPurple,
-                        padding: const EdgeInsets.all(8),
-                        child: Text(
-                          radicalGroups[groupIndex].strokes == 1
-                              ? '${radicalGroups[groupIndex].strokes} stroke'
-                              : '${radicalGroups[groupIndex].strokes} strokes',
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.white),
-                        ),
-                      ),
-                      Wrap(children: radicalGroups[groupIndex].radicals),
-                    ],
+          body: ListView.builder(
+            itemCount: radicalGroups.length,
+            itemBuilder: (context, groupIndex) => Padding(
+              padding: const EdgeInsets.only(bottom: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Colors.deepPurple,
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      radicalGroups[groupIndex].strokes == 1
+                          ? '${radicalGroups[groupIndex].strokes} stroke'
+                          : '${radicalGroups[groupIndex].strokes} strokes',
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
                   ),
-                ),
+                  Wrap(children: radicalGroups[groupIndex].radicals),
+                ],
               ),
-              SizedBox(height: MediaQuery.of(context).padding.bottom),
-            ],
+            ),
           ),
         );
       },
