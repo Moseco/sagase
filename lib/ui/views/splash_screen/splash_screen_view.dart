28e2884632410e522d3f9521db3e8af82bb64418
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2023-11-24 13:21:18 +0900

    Improved splash screen status ui

--- lib/ui/views/splash_screen/splash_screen_view.dart
+++ lib/ui/views/splash_screen/splash_screen_view.dart
@@ -18,107 +18,103 @@ class SplashScreenView extends StackedView<SplashScreenViewModel> {
           padding: const EdgeInsets.all(8.0),
           child: switch (viewModel.status) {
             SplashScreenStatus.waiting => Container(),
-            SplashScreenStatus.downloadingAssets => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: Text(
-                      'Downloading dictionary',
-                      textAlign: TextAlign.center,
-                    ),
+            SplashScreenStatus.downloadingAssets => _Body(
+                title: 'Downloading Assets',
+                widget: CircularPercentIndicator(
+                  radius: 40,
+                  lineWidth: 8,
+                  animation: true,
+                  animateFromLastPercent: true,
+                  percent: viewModel.downloadStatus,
+                  center: Text(
+                    "${(viewModel.downloadStatus * 100).toInt()}%",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  CircularPercentIndicator(
-                    radius: 40,
-                    lineWidth: 8,
-                    animation: true,
-                    animateFromLastPercent: true,
-                    percent: viewModel.downloadStatus,
-                    center: Text(
-                      "${(viewModel.downloadStatus * 100).toInt()}%",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    circularStrokeCap: CircularStrokeCap.round,
-                    progressColor: Colors.deepPurple,
-                  ),
-                ],
+                  circularStrokeCap: CircularStrokeCap.round,
+                  progressColor: Colors.deepPurple,
+                ),
               ),
-            SplashScreenStatus.upgradingDictionary => const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: Text(
-                      'Upgrading dictionary. Please wait.\nThis should take less than 30 seconds.',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  CircularProgressIndicator(),
-                ],
+            SplashScreenStatus.upgradingDictionary => const _Body(
+                title: 'Upgrading Dictionary',
+                subtitle: 'This should be pretty quick.',
+                widget: CircularProgressIndicator(),
               ),
-            SplashScreenStatus.downloadError => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: Text(
-                      'Failed to download the dictionary.\nMake sure you are connected to the internet and try again.\nIf it keeps happening try updating the app.',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: viewModel.startDownload,
-                    child: const Text('Retry download'),
-                  ),
-                ],
+            SplashScreenStatus.downloadError => _Body(
+                title: 'Download Failed',
+                subtitle:
+                    'Make sure you are connected to the internet and try again.\nIf it keeps happening try updating the app.',
+                widget: ElevatedButton(
+                  onPressed: viewModel.startDownload,
+                  child: const Text('Retry download'),
+                ),
               ),
-            SplashScreenStatus.databaseError => const Text(
-                'Something went wrong with the dictionary.\nPlease try closing the app and reopening it.',
-                textAlign: TextAlign.center,
+            SplashScreenStatus.databaseError => const _Body(
+                title: 'Dictionary Error',
+                subtitle:
+                    'Something went wrong with the dictionary.\nPlease try closing the app and reopening it.',
               ),
-            SplashScreenStatus.downloadRequest => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16),
-                    child: Text(
-                      'There is a dictionary upgrade available.\nPlease keep the app open during the upgrade; it should take less then a minute.',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: viewModel.startDownload,
-                    child: const Text('Start download'),
-                  ),
-                ],
+            SplashScreenStatus.downloadRequest => _Body(
+                title: 'Upgrade Available',
+                subtitle:
+                    'There is a new version of the dictionary available.\nPlease keep the app open during the upgrade.\nIt should take less then a minute.',
+                widget: ElevatedButton(
+                  onPressed: viewModel.startDownload,
+                  child: const Text('Start download'),
+                ),
               ),
-            SplashScreenStatus.dictionaryUpgradeError => const Text(
-                'Something went wrong while upgrading the dictionary.\nPlease try closing the app and reopening it.',
-                textAlign: TextAlign.center,
+            SplashScreenStatus.dictionaryUpgradeError => const _Body(
+                title: 'Upgrade Failed',
+                subtitle:
+                    'Something went wrong while upgrading the dictionary.\nPlease try closing the app and reopening it.',
               ),
-            _ => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      'Preparing the dictionary. Please wait.\nThis should take less than 30 seconds.',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: Text(
-                      'Completed ${viewModel.status.index - SplashScreenStatus.importingDictionary.index}/3',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const CircularProgressIndicator(),
-                ],
+            _ => _Body(
+                title: 'Finishing Setup',
+                subtitle:
+                    'Completed ${viewModel.status.index - SplashScreenStatus.importingDictionary.index}/3',
+                widget: const CircularProgressIndicator(),
               ),
           },
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final Widget? widget;
+
+  const _Body({
+    required this.title,
+    this.subtitle,
+    this.widget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 24),
+        ),
+        if (subtitle != null)
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Text(
+              subtitle!,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        if (widget != null)
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: widget,
+          ),
+      ],
     );
   }
 }
