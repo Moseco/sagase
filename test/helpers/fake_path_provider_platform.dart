e55290aaa9ae70bb327067f770c742b9184c237e
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2024-02-16 11:54:18 +0900

    test: update fake path provider

--- test/helpers/fake_path_provider_platform.dart
+++ test/helpers/fake_path_provider_platform.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:path/path.dart';
 
 class FakePathProviderPlatform extends Fake
     with MockPlatformInterfaceMixin
@@ -10,44 +11,38 @@ class FakePathProviderPlatform extends Fake
   final String basePath;
 
   FakePathProviderPlatform(this.basePath);
-  @override
-  Future<String?> getTemporaryPath() async {
-    String path = '$basePath${Platform.pathSeparator}temporary';
-    await Directory(path).create(recursive: true);
-    return path;
-  }
 
   @override
-  Future<String?> getApplicationSupportPath() async {
-    String path = '$basePath${Platform.pathSeparator}applicationSupport';
-    await Directory(path).create(recursive: true);
-    return path;
-  }
-
-  @override
-  Future<String?> getLibraryPath() async {
-    String path = '$basePath${Platform.pathSeparator}library';
+  Future<String?> getApplicationCachePath() async {
+    String path = join(basePath, 'applicationCache');
     await Directory(path).create(recursive: true);
     return path;
   }
 
   @override
   Future<String?> getApplicationDocumentsPath() async {
-    String path = '$basePath${Platform.pathSeparator}applicationDocuments';
+    String path = join(basePath, 'applicationDocuments');
     await Directory(path).create(recursive: true);
     return path;
   }
 
   @override
-  Future<String?> getExternalStoragePath() async {
-    String path = '$basePath${Platform.pathSeparator}externalStorage';
+  Future<String?> getApplicationSupportPath() async {
+    String path = join(basePath, 'applicationSupport');
+    await Directory(path).create(recursive: true);
+    return path;
+  }
+
+  @override
+  Future<String?> getDownloadsPath() async {
+    String path = join(basePath, 'downloads');
     await Directory(path).create(recursive: true);
     return path;
   }
 
   @override
   Future<List<String>?> getExternalCachePaths() async {
-    String path = '$basePath${Platform.pathSeparator}externalCache';
+    String path = join(basePath, 'externalCache');
     await Directory(path).create(recursive: true);
     return [path];
   }
@@ -56,14 +51,28 @@ class FakePathProviderPlatform extends Fake
   Future<List<String>?> getExternalStoragePaths({
     StorageDirectory? type,
   }) async {
-    String path = '$basePath${Platform.pathSeparator}externalStorage';
+    String path = join(basePath, 'externalStorage');
     await Directory(path).create(recursive: true);
     return [path];
   }
 
   @override
-  Future<String?> getDownloadsPath() async {
-    String path = '$basePath${Platform.pathSeparator}downloads';
+  Future<String?> getExternalStoragePath() async {
+    String path = join(basePath, 'externalStorage');
+    await Directory(path).create(recursive: true);
+    return path;
+  }
+
+  @override
+  Future<String?> getLibraryPath() async {
+    String path = join(basePath, 'library');
+    await Directory(path).create(recursive: true);
+    return path;
+  }
+
+  @override
+  Future<String?> getTemporaryPath() async {
+    String path = join(basePath, 'temporary');
     await Directory(path).create(recursive: true);
     return path;
   }
