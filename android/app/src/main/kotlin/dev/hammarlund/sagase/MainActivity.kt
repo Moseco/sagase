f390d66a36abc4110626a8315f002e53a76dff5b
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2024-02-22 14:53:52 +0900

    bugfix: prevent other apps from opening new instances of the app on Android

--- android/app/src/main/kotlin/dev/hammarlund/sagase/MainActivity.kt
+++ android/app/src/main/kotlin/dev/hammarlund/sagase/MainActivity.kt
@@ -1,6 +1,17 @@
 package dev.hammarlund.sagase
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Prevent other apps from creating new instances of this activity
+        if (!isTaskRoot()) {
+            this.finish()
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+        super.onCreate(savedInstanceState)
+    }
 }
