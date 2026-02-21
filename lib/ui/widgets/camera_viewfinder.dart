109bb626e9b4eb56cdca3549531e1960893c2bb9
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2026-02-21 22:11:34 +0900

    fix: add better error handling to camera viewfinder

--- lib/ui/widgets/camera_viewfinder.dart
+++ lib/ui/widgets/camera_viewfinder.dart
@@ -16,9 +16,8 @@ class CameraViewfinder extends StatefulWidget {
 class _CameraViewfinderState extends State<CameraViewfinder>
     with WidgetsBindingObserver {
   CameraController? _controller;
-  List<CameraDescription>? _cameras;
-  bool _cameraInitialized = false;
-  bool _cameraError = false;
+  late List<CameraDescription> _cameras;
+  CameraState _cameraState = CameraState.uninitialized;
 
   @override
   void initState() {
@@ -27,40 +26,45 @@ class _CameraViewfinderState extends State<CameraViewfinder>
   }
 
   Future<void> _initCamera() async {
-    _cameras = await availableCameras();
-    if (_cameras!.isEmpty) {
-      setState(() {
-        _cameraError = true;
-      });
-    }
-    CameraDescription cameraToUse = _cameras!.first;
-    for (final camera in _cameras!) {
-      if (camera.lensDirection == CameraLensDirection.back) {
-        cameraToUse = camera;
-        break;
+    try {
+      _cameras = await availableCameras();
+      if (_cameras.isEmpty) {
+        setState(() => _cameraState = CameraState.permissionDenied);
+        return;
       }
-    }
-
-    // This is a temporary workaround for iPhone 17 family devices
-    final iOS = Platform.isIOS ? await DeviceInfoPlugin().iosInfo : null;
-
-    _controller = CameraController(
-      cameraToUse,
-      iOS != null && iOS.utsname.machine.contains("iPhone18")
-          ? ResolutionPreset.ultraHigh
-          : ResolutionPreset.max,
-      enableAudio: false,
-    );
-
-    _controller!.initialize().then((_) {
-      if (mounted) {
-        setState(() => _cameraInitialized = true);
+      CameraDescription cameraToUse = _cameras.first;
+      for (final camera in _cameras) {
+        if (camera.lensDirection == CameraLensDirection.back) {
+          cameraToUse = camera;
+          break;
+        }
       }
-    }).catchError((Object e) {
-      setState(() {
-        _cameraError = true;
+
+      // This is a temporary workaround for iPhone 17 family devices
+      final iOS = Platform.isIOS ? await DeviceInfoPlugin().iosInfo : null;
+
+      _controller = CameraController(
+        cameraToUse,
+        iOS != null && iOS.utsname.machine.contains("iPhone18")
+            ? ResolutionPreset.ultraHigh
+            : ResolutionPreset.max,
+        enableAudio: false,
+      );
+
+      _controller!.initialize().then((_) {
+        if (mounted) {
+          setState(() => _cameraState = CameraState.initialized);
+        }
+      }).catchError((Object e) {
+        if (e is CameraException && e.code == 'CameraAccessDenied') {
+          setState(() => _cameraState = CameraState.permissionDenied);
+        } else {
+          setState(() => _cameraState = CameraState.error);
+        }
       });
-    });
+    } catch (e) {
+      setState(() => _cameraState = CameraState.error);
+    }
   }
 
   @override
@@ -76,7 +80,7 @@ class _CameraViewfinderState extends State<CameraViewfinder>
     }
 
     if (state == AppLifecycleState.inactive) {
-      setState(() => _cameraInitialized = false);
+      setState(() => _cameraState = CameraState.uninitialized);
       _controller!.dispose();
     } else if (state == AppLifecycleState.resumed) {
       _initCamera();
@@ -84,60 +88,84 @@ class _CameraViewfinderState extends State<CameraViewfinder>
   }
 
   Future<void> _takePhoto() async {
-    if (!_controller!.value.isInitialized) return;
-    final image = await _controller!.takePicture();
-    widget.onPictureTaken(image);
+    try {
+      if (!_controller!.value.isInitialized) return;
+      final image = await _controller!.takePicture();
+      widget.onPictureTaken(image);
+    } catch (e) {
+      setState(() => _cameraState = CameraState.error);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_cameraError) {
-      return const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 4,
+    switch (_cameraState) {
+      case CameraState.uninitialized:
+        return const Center(child: CircularProgressIndicator());
+      case CameraState.initialized:
+        return Stack(
           children: [
-            Text(
-              'Failed to start camera',
-              style: TextStyle(fontSize: 16),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: FittedBox(
+                clipBehavior: Clip.hardEdge,
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: CameraPreview(_controller!),
+                ),
+              ),
             ),
-            Text('Confirm camera permissions in system settings'),
+            Positioned(
+              bottom: MediaQuery.of(context).padding.bottom + 16,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: FloatingActionButton(
+                  onPressed: _takePhoto,
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.deepPurple,
+                  child: const Icon(Icons.camera_alt),
+                ),
+              ),
+            ),
           ],
-        ),
-      );
-    }
-
-    if (_cameraInitialized) {
-      return Stack(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: FittedBox(
-              clipBehavior: Clip.hardEdge,
-              fit: BoxFit.cover,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: CameraPreview(_controller!),
+        );
+      case CameraState.permissionDenied:
+        return const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 4,
+            children: [
+              Text(
+                'Failed to start camera',
+                style: TextStyle(fontSize: 16),
               ),
-            ),
+              Text('Confirm camera permissions in system settings'),
+            ],
           ),
-          Positioned(
-            bottom: MediaQuery.of(context).padding.bottom + 16,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: FloatingActionButton(
-                onPressed: _takePhoto,
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.deepPurple,
-                child: const Icon(Icons.camera_alt),
+        );
+      case CameraState.error:
+        return const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 4,
+            children: [
+              Text(
+                'Something went wrong',
+                style: TextStyle(fontSize: 16),
               ),
-            ),
+              Text('Please try again later'),
+            ],
           ),
-        ],
-      );
+        );
     }
-
-    return const Center(child: CircularProgressIndicator());
   }
+}
+
+enum CameraState {
+  uninitialized,
+  initialized,
+  permissionDenied,
+  error,
 }
